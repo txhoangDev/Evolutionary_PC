7fb695f6cca7f21128f32c4da0cf63622bc27ea7
Thuong Hoang <thuonghoang@Thuongs-Air.lan>
2022-09-13 18:36:16 -0500

    formatted the json files

--- data_initialization_scripts/data_init.py
+++ data_initialization_scripts/data_init.py
@@ -50,7 +50,7 @@ def get_cpu_price(item_name):
 
 if __name__ == "__main__":
     # initalizes an API instance and sets region to US
-    with open("Data.json", 'r') as file:
+    with open("GPU.json", 'r') as file:
         data = json.load(file)
     file.close()
     # with open("Data.json", 'w') as json_file:
@@ -58,16 +58,16 @@ if __name__ == "__main__":
     #     # y, m, d = [int(x) for x in data['Last_Update'].split("-")]
     #     # last_update = date(y, m, d)
     # json_file.close()
-    api_instance = API()
-    parts_data = api_instance.retrieve_all()
-    for part in parts_data:
-        if part == 'cpu':
-            for info in parts_data[part]:
-                data[part].append({"name": info.brand + ' ' + info.model, "price": info.price.__str__(), "benchmark": 0})
-        elif part == 'video-card':
-            for info in parts_data[part]:
-                data[part].append({"name": info.brand + ' ' + info.model, "price": info.price.__str__(), "benchmark": 0})
+    # api_instance = API()
+    # parts_data = api_instance.retrieve_all()
+    # for part in parts_data:
+    #     if part == 'cpu':
+    #         for info in parts_data[part]:
+    #             data[part].append({"name": info.brand + ' ' + info.model, "price": info.price.__str__(), "benchmark": 0})
+    #     elif part == 'video-card':
+    #         for info in parts_data[part]:
+    #             data[part].append({"name": info.brand + ' ' + info.model, "price": info.price.__str__(), "benchmark": 0})
     info = json.dumps(data, indent=4)
-    with open("Data.json", "w") as file:
+    with open("GPU.json", "w") as file:
         file.write(info)
     file.close()
