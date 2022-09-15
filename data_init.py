998535cd4d227fbd555ab38bdbd4a9448b3d9fbb
Thuong Hoang <thuonghoang@Thuongs-Air.lan>
2022-09-14 22:33:10 -0500

    testing evolutionary algorithm

--- data_init.py
+++ data_init.py
@@ -1,8 +1,10 @@
 # imports
 import json
 from datetime import date
-from Data_Scraper.scraper import Scraper
+# from Data_Scraper.scraper import Scraper
 from evolutionary_algorithm.Test import PC_Build
+import pygad
+import numpy
 
 """
 Some pcpartpicker supported parts:
@@ -17,11 +19,16 @@ Some pcpartpicker supported parts:
     HD = 'external-hard-drive'
 """
 
+# initialize number of combinations
+with open("Data.json", "r") as file:
+    parts_data = json.load(file)
+file.close()
+
+def calculate_fitness(solution, solution_index):
+    fitness_score = parts_data['cpu'][solution[solution_index][0]]+ parts_data['gpu'][solution[solution_index][1]]
+    return fitness_score
+
 if __name__ == "__main__":
-    # initialize number of combinations
-    with open("Data.json", "r") as file:
-        parts_data = json.load(file)
-    file.close()
     # gets the list of available CPUs
     cpu_list = parts_data['cpu'].keys()
     # gets the list of available GPUs
@@ -29,5 +36,25 @@ if __name__ == "__main__":
     # builds a set of combinations of both
     builds = list(zip(cpu_list, gpu_list))
     
-    new_build = PC_Build()
-    print(new_build.Calculate_Fitness(builds))
+    temp = PC_Build()
+    
+    temp.run(builds, 50)
+
+    # num_gen = 100
+    # num_parents = 10
+    # sol_per_pop = 20
+    # num_genes = 2
+    
+    # last_fiteness = 0
+    
+    # ga_instance = pygad.GA(num_generations=num_gen,
+    #                        num_parents_mating=num_parents,
+    #                        initial_population=builds,
+    #                        sol_per_pop=sol_per_pop,
+    #                        num_genes = num_genes,
+    #                        crossover_type="single_point",
+    #                         fitness_func=calculate_fitness)
+    
+    # ga_instance.run()
+    
+    # ga_instance.plot_fitness()
--- evolutionary_algorithm/Test.py
+++ evolutionary_algorithm/Test.py
@@ -7,9 +7,37 @@ class PC_Build:
         parts_data = json.load(file)
     file.close()
     
+    fitness_scores = []
+    build_fitness_map = {}
+    
     def Calculate_Fitness(self, builds):
-        fitness_score_list = []
         for build in builds:
             fitness_score = self.parts_data['cpu'][build[0]]['benchmark'] + self.parts_data['gpu'][build[1]]['benchmark']
-            fitness_score_list.append(fitness_score)
-        return fitness_score_list
+            self.fitness_scores.append(fitness_score)
+    
+    def Get_Parents(self, builds, fitness_scores):
+        for ii in range(len(builds)):
+            self.build_fitness_map[fitness_scores[ii]] = builds[ii]
+        fitness_scores.sort(reverse=True)
+        return self.build_fitness_map[fitness_scores[0]], self.build_fitness_map[fitness_scores[1]]
+    
+    def Generate_Children(self, parents, builds):
+        cpus = [parents[0][0], parents[1][0]]
+        gpus = [parents[0][1], parents[1][1]]
+        children = list(zip(cpus, gpus))
+        child_score = self.Calculate_Fitness(children)
+        self.fitness_scores.sort()
+        for score in range(len(children)):
+            if self.current_fitness_scores[0] < score:
+                self.build_fitness_map.pop(self.current_fitness_scores[0])
+                self.current_fitness_scores[0] = child_score[score]
+                self.build_fitness_map.pop[self.current_fitness_scores[0]] = children[score]
+                self.current_fitness_scores.sort()
+                
+    def run(self, builds, num_of_generations):
+        for ii in range(num_of_generations):
+            self.Calculate_Fitness(builds)
+            parents = self.Generate_Children(builds, self.fitness_scores)
+            self.Generate_Children(parents, builds)
+        self.fitness_scores.sort(reverse=True)
+        return self.build_fitness_map[self.fitness_scores[0]]
